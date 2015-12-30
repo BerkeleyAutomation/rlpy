586e20dd74ffa07238542e0582fb47192db7941b
Richard Liaw <rliaw@berkeley.edu>
2015-12-29 19:48:26 -0800

    New Experiment Files

--- rlpy/Experiments/__init__.py
+++ rlpy/Experiments/__init__.py
@@ -1,4 +1,6 @@
 from .Experiment import Experiment
+from .ExperimentSegment import ExperimentSegment
+from .ExperimentDelayed import ExperimentDelayed
 from .MDPSolverExperiment import MDPSolverExperiment
 # for backward compatibility with existing experiment scripts
 OnlineExperiment = Experiment
